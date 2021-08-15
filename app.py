9aa9789d9f3fb3c0d32eaa6f569b8f189c46c0af
manjiri13 <52316816+manjiri13@users.noreply.github.com>
2021-08-16 00:56:35 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-# from flask import Flask, render_template, request
+
 import jsonify
 import requests
 import time
@@ -89,78 +89,4 @@ with st.form("my_form"):
             with st.container():
                 st.write(fuck[i],'% of comments are of emotion ', emoo[i])
 
-# app = Flask(__name__)
-# model = load_model('model.h5')
 
-# @app.route('/',methods=['GET'])
-# def Home():
-#     return render_template('index.html')
-
-
-# @app.route("/predict", methods=['POST'])
-# def predict():
-#     if request.method == 'POST':
-#         link = request.form['Link']
-#         data=[]
-#         with Chrome(executable_path=r'/home/unknown/Documents/chromedriver') as driver:
-#             wait = WebDriverWait(driver,15)
-#             driver.get(link)
-#             time.sleep(5)
-#             driver.execute_script('window.scrollTo(1, 100);')
-#             time.sleep(5)
-#             for item in range(20): 
-#                 wait.until(EC.visibility_of_element_located((By.TAG_NAME, "body"))).send_keys(Keys.END)
-#                 time.sleep(2)
-
-#             for comment in wait.until(EC.presence_of_all_elements_located((By.CSS_SELECTOR, "#content-text"))):
-#                 data.append(comment.text)
-
-#             df = pd.DataFrame(data, columns=['Comment'])
-#             df.drop(index=[0,1,2], axis=0, inplace=True)
-#             df["length"] = [len(i) for i in df["Comment"]]
-#             df1=df[df["length"] > 300].index
-#             df.drop(df1,axis=0,inplace=True)
-#             stopwords = set(nltk.corpus.stopwords.words('english'))
-#             vocab_size=10000
-#             len_sentence=150
-#             def text_prepare(data, column):
-#                 print(data.shape)
-#                 stemmer = PorterStemmer()
-#                 corpus = []
-                
-#                 for text in data[column]:
-                    
-#                     text = re.sub("[^a-zA-Z]", " ", text)
-                    
-#                     text = text.lower()
-#                     text = text.split()
-                    
-#                     text = [stemmer.stem(word) for word in text if word not in stopwords]
-#                     text = " ".join(text)
-                    
-#                     corpus.append(text)
-#                 one_hot_word = [one_hot(input_text=word, n=vocab_size) for word in corpus]
-#                 embeddec_doc = pad_sequences(sequences=one_hot_word,
-#                                         maxlen=len_sentence,
-#                                         padding="pre")
-#                 print(data.shape)
-#                 return embeddec_doc
-#             emo=text_prepare(df, "Comment")
-#             kk= model.predict(emo)
-#             output=np.argmax(kk, axis=1)
-#             output=output.tolist()
-#             e={0:"anger",1:"fear",2:"joy",3:"love",4:"sadness",5:"surprise"}
-#             fuck=[]
-#             for i in range(6):
-#                fuck.append(int(output.count(i)*100/len(output)))
-
-#             return render_template('index.html',prediction_text='''{} % of the comments are of emotion anger,\n 
-#             {} % of the comments are of emotion fear,\n
-#             {} % of the comments are of emotion joy,\n
-#             {} % of the comments are of emotion love,\n
-#             {} % of the comments are of emotion sadness,\n
-#             {} % of the comments are of emotion surprise,\n
-#             '''.format(fuck[0],fuck[1],fuck[2],fuck[3],fuck[4],fuck[5]))
-
-# if __name__=="__main__":
-#     app.run(debug=True)
